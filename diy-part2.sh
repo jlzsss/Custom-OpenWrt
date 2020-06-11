3cbe69d8da3fa4d2785e00a7fb5ed00d2e8d6c3d
jlzsss <jlz19821031@gmail.com>
2020-06-11 14:22:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,8 +23,8 @@ git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR.git package/luci-a
 # git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jlzsss/luci-app-passwall.git package/luci-app-passwall
 # git clone https://github.com/jlzsss/openwrt-ssr-libev-full.git package/openwrt-ssr-libev-full
-git clone https://github.com/jlzsss/luci-app-shadowsocksr.git package/luci-app-shadowsocksr
-# git clone https://github.com/jlzsss/openwrt-ssr.git package/openwrt-ssr
+# git clone https://github.com/jlzsss/luci-app-shadowsocksr.git package/luci-app-shadowsocksr
+git clone https://github.com/jlzsss/openwrt-ssr.git package/openwrt-ssr
 	
 ./scripts/feeds update -a
 ./scripts/feeds install -a
