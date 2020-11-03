9b911e1e5b2762484ef4b7a3109d433d27ed4fdc
jlzsss <jlz19821031@gmail.com>
2020-11-03 20:34:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,6 +19,7 @@
 
 git clone https://github.com/brvphoenix/wrtbwmon.git package/wrtbwmon
 git clone https://github.com/brvphoenix/luci-app-wrtbwmon.git package/luci-app-wrtbwmon
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 svn co https://github.com/openwrt/packages/trunk/net/dawn package/feeds/packages/dawn
 svn co https://github.com/openwrt/packages/trunk/net/vnstat2 package/feeds/packages/vnstat2
 svn co https://github.com/openwrt/packages/trunk/net/openfortivpn package/feeds/packages/openfortivpn
