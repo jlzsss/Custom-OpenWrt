48a099e080992a33bf46e3971a1643586a7023f8
jlzsss <jlz19821031@gmail.com>
2020-12-06 12:14:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,9 +26,9 @@ git clone -b test https://github.com/frainzy1477/luci-app-clash.git package/luci
 git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/luci-app-koolproxyR
 git clone -b xray-111 https://github.com/yichya/luci-app-xray.git package/luci-app-xray
+git clone https://github.com/jlzsss/luci-app-ssr-plus-Jo.git package/luci-app-ssr-plus-Jo
+git clone https://github.com/jlzsss/luci-app-ssr-plus-jo-depend.git package/luci-app-ssr-plus-jo-depend
 svn co https://github.com/openwrt/packages/trunk/libs/libcap package/feeds/packages/libcap
-# git clone https://github.com/jlzsss/luci-app-ssr-plus-Jo.git package/luci-app-ssr-plus-Jo
-# git clone https://github.com/jlzsss/luci-app-ssr-plus-jo-depend.git package/luci-app-ssr-plus-jo-depend
 # git clone https://github.com/jlzsss/dnscrypt-proxy2.git package/feeds/packages/dnscrypt-proxy2
 # git clone https://github.com/jlzsss/smartdns.git package/feeds/packages/smartdns
 # git clone https://github.com/jlzsss/luci-app-smartdns.git package/feeds/luci/luci-app-smartdns
