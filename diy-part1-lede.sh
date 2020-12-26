9217382c16b0516822a7fa49840d6878a48cdfe8
jlzsss <jlz19821031@gmail.com>
2020-12-26 16:26:15 +0800

    Update diy-part1-lede.sh

--- diy-part1-lede.sh
+++ diy-part1-lede.sh
@@ -17,8 +17,8 @@
 git clone https://github.com/kuoruan/luci-app-kcptun.git package/luci-app-kcptun
 git clone https://github.com/kuoruan/openwrt-kcptun.git package/kcptun
 git clone https://github.com/jlzsss/openwrt-miredo.git package/miredo
-# git clone https://gitee.com/wangfuying_admin/Docker-For-OpenWRT.git package/docker
-# git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+git clone https://gitee.com/wangfuying_admin/Docker-For-OpenWRT.git package/docker
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 git clone https://github.com/jlzsss/proxychains-ng.git package/proxychains-ng
 git clone https://github.com/jlzsss/luci-app-xray.git package/luci-app-xray
 git clone -b luci2 https://github.com/jlzsss/luci-app-v2ray.git package/luci-app-v2ray
