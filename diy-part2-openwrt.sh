f3e9f2fe0b7eb9c3bd316b4239e307af10588d6c
jlzsss <jlz19821031@gmail.com>
2021-01-08 11:23:20 +0800

    Update diy-part2-openwrt.sh

--- diy-part2-openwrt.sh
+++ diy-part2-openwrt.sh
@@ -16,9 +16,10 @@
 # git clone https://github.com/jlzsss/luci-app-passwall.git package/luci-app-passwall
 # git clone https://github.com/jlzsss/openwrt-ssr-libev-full.git package/openwrt-ssr-libev-full
 # git clone https://github.com/jlzsss/openwrt-ssr.git package/openwrt-ssr
-# git clone https://github.com/brvphoenix/wrtbwmon.git package/wrtbwmon
-# git clone https://github.com/brvphoenix/luci-app-wrtbwmon.git package/luci-app-wrtbwmon
 
+rm -rf package/lean/luci-app-wrtbwmon
+git clone https://github.com/brvphoenix/wrtbwmon.git package/wrtbwmon
+git clone https://github.com/brvphoenix/luci-app-wrtbwmon.git package/luci-app-wrtbwmon
 git clone https://github.com/jlzsss/luci-app-shadowsocksr.git package/luci-app-shadowsocksr
 git clone https://github.com/jlzsss/openwrt-dnsmasq-extra.git package/openwrt-dnsmasq-extra
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
