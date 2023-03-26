8e46f9e18e34f260e6ad48bb64f46a40c727b12e
jlzsss <jlz19821031@gmail.com>
2023-03-26 08:39:41 +0800

    Update diy-part1-openwrt.sh

--- diy-part1-openwrt.sh
+++ diy-part1-openwrt.sh
@@ -46,7 +46,7 @@ git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-git clone https://github.com/kiddin9/qBittorrent-Enhanced-Edition package/feeds/packages/qBittorrent-Enhanced-Edition
+git clone https://github.com/jlzsss/qBittorrent-Enhanced-Edition.git package/feeds/packages/qBittorrent-Enhanced-Edition
 
 
 
